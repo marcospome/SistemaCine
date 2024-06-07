bf805a6d34647fc151ab90c778b36999c93030eb
mpomeranietz <mpomeranietz@ITL0593>
2024-06-07 17:45:22 -0300

    Agregar archivos de proyecto.

---
+++ semana4/Models/Funcion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;  // For ICollection
+
+namespace semana4.Models
+{
+    public class Funcion
+    {
+        [Key]
+        public int IdFuncion { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Dia { get; set; }
+
+        [Required]
+        public TimeSpan Horario { get; set; }
+
+        [StringLength(50)]
+        public string Lenguaje { get; set; }
+
+        [Required]  // Marca IdPelicula como obligatorio
+        public int IdPelicula { get; set; } // Clave foránea
+    }
+}
